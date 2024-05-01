5412a9ceb098e7fc7390b885aa4fdf6fc2e206d8
AbebeGetnet <aabizy@gmail.com>
2024-05-01 04:42:57 -0700

    Add Direct charge openning and charge follow up

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using ExpertManagmentSystem.ViewModels;
 using ExpertManagmentSystem.Models.CrimeModels;
+using ExpertManagmentSystem.Models.CivilCaseModels;
 
 namespace ExpertManagmentSystem.Data
 {
@@ -26,6 +27,8 @@ namespace ExpertManagmentSystem.Data
         public DbSet<Cr_Crime_Type> Cr_Crime_Types { get; set; }
         public DbSet<Cr_Decided_Judicial_and_Prosecuter> Cr_Decided_Judicial_and_Prosecuters { get; set; }
         public DbSet<Cr_JudicalAppealDirectCharege> Cr_JudicalAppealDirectChareges { get; set; }
+        public DbSet<DirectChargeOpenning> DirectChargeOpennings { get; set; }
+        public DbSet<DirectChargeFollowUp> DirectChargeFollowUps { get; set; }
 
 
 
---
+++ Models/Audit.cs
@@ -0,0 +1,15 @@
+﻿using ExpertManagmentSystem.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpertManagmentSystem.Models
+{
+    public class Audit
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime EdittedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public string ApplicationUserUser { get; set; }
+        public virtual ApplicationUser? ApplicationUser { get; set; }
+    }
+}
